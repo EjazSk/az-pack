35982e9ef75641ffd5e0fd529d0628e52139a618
Ejaz Shaikh <Ejaz.Shaikh@blueconchtech.com>
2021-05-16 20:19:36 +0530

    new

--- actions/all.py
+++ actions/all.py
@@ -3,18 +3,20 @@ import json
 from st2common.runners.base_action import Action
 
 
-__all__ = [
-    'All'
-]
+__all__ = ["All"]
+
 
 class All(Action):
+    BASE_URL = "https://jsonplaceholder.typicode.com/todos"
 
     def __init__(self, config) -> None:
-        super(All,self).__init__(config)
+        super(All, self).__init__(config)
 
-        self.url = config.get('base_url')
+        self.url = config.get("base_url")
 
-    
     def run(self):
-        result = requests.get(self.url).json()
+        if self.url:
+            result = requests.get(self.url).json()
+        else:
+            result = requests.get(self.BASE_URL).json()
         return result
