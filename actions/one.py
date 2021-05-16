1f9b263eca644906f655d9684cea7d0d598d4daf
Ejaz Shaikh <Ejaz.Shaikh@blueconchtech.com>
2021-05-16 20:41:46 +0530

    new

--- actions/one.py
+++ actions/one.py
@@ -11,7 +11,7 @@ class One(Action):
     def __init__(self, config) -> None:
         super(One, self).__init__(config)
 
-        self.id = self.config.get("id")
+        self.url = self.config.get("id")
 
     def run(self, id):
         if self.url:
